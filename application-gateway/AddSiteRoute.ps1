54f71a555434818ae86c65da066defa8310b9fee
fbeltrao <fbeltrao@windowslive.com>
2018-06-27 11:29:13 +0200

    added deploy to azure button

---
+++ application-gateway/AddSiteRoute.ps1
@@ -0,0 +1,105 @@
+<#
+ .SYNOPSIS
+    Adds a Azure Web Site or External URI route to an existing Application Gateway
+
+ .PARAMETER resourceGroupName
+    The resource group where the Application Gateway and the Web Site are deployed
+
+ .PARAMETER applicationGatewayName
+    The Application Gateway name
+
+ .PARAMETER webSiteName
+    The Azure web site name
+
+ .PARAMETER webSiteResourceGroupName
+    The resource group where the webSite is located. If empty will take the value of resourceGroupName parameter
+
+ .PARAMETER externalURI
+    The external URI to be mapped
+
+ .PARAMETER paths
+    The paths that will match the APIs. Example "/api/myresource","/api/myresource/*"
+#>
+
+param(
+ [Parameter(Mandatory=$True)]
+ [string]  $resourceGroupName,
+
+ [Parameter(Mandatory=$True)]
+ [string] $applicationGatewayName,
+
+ 
+ [string] $externalURI,
+ [string] $webSiteResourceGroupName,
+ [string] $webSiteName,
+
+ [Parameter(Mandatory=$True)]
+ [string[]] $paths
+)
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$ErrorActionPreference = "Stop"
+
+# auto-select subscription
+$subscriptionId = (Get-AzureRmContext).Subscription.Id;
+
+if (!$subscriptionId)
+{
+    Connect-AzureRmAccount
+    #Write-Host "Could not resolve Azure Subscription ID. Make sure your are logged-in 'Connect-AzureRmAccount'"
+    $subscriptionId = (Get-AzureRmContext).Subscription.Id;
+}
+
+# retrieve the Web Site
+$backendFqns = $externalURI
+$backendPoolName = ""
+$ruleName = ""
+if ($webSiteName)
+{
+    if (!$webSiteResourceGroupName)
+    {
+        $webSiteResourceGroupName = $resourceGroupName
+    }
+
+    $webSite = Get-AzureRmWebApp -ResourceGroupName $webSiteResourceGroupName -Name $webSiteName
+    $backendFqns = $webSite.HostNames
+
+    $backendPoolName = $webSite.Name
+    $backEndPoolName += "BackendPool"
+    $ruleName = $webSite.Name
+    $ruleName += "Rule"
+}
+else 
+{    
+    $safeNameFromExternalURI = $externalURI -replace '[^a-zA-Z0-9]', ''
+    $ruleName = $safeNameFromExternalURI
+    $ruleName += "Rule"
+
+    $backendPoolName = $safeNameFromExternalURI
+    $backendPoolName += "BackendPool"
+
+}
+
+
+
+# retrieve the Application Gateway
+$appGateway = Get-AzureRmApplicationGateway -ResourceGroupName $resourceGroupName -Name $applicationGatewayName
+
+
+# create new backendPool
+$appGateway = Add-AzureRmApplicationGatewayBackendAddressPool -ApplicationGateway $appGateway -Name $backEndPoolName -BackendFqdns $backendFqns
+
+# get backend http settings to use (for now get the first)
+$backendHttpSetting = $appGateway.BackendHttpSettingsCollection[0]
+
+# create path rule
+$pathRule = New-AzureRmApplicationGatewayPathRuleConfig -Name $ruleName -Paths $paths -BackendAddressPool $appGateway.BackendAddressPools[-1] -BackendHttpSettings $backendHttpSetting
+
+# insert the rule
+$appGateway.UrlPathMaps[0].PathRules = , $pathRule + $appGateway.UrlPathMaps[0].PathRules
+
+# apply application gateway changes
+Set-AzureRmApplicationGateway -ApplicationGateway $appGateway
--- application-gateway/README.md
+++ application-gateway/README.md
@@ -2,6 +2,24 @@
 
 This template will deploy an Application Gateway using by default a Path-based routing instead of the "Basic" created by Azure Portal.
 
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Ffbeltrao%2Fazdeploy%2Fmaster%2Fapplication-gateway%2Ftemplate.json" target="_blank">
+    <img src="https://azuredeploy.net/deploybutton.png"/>
+</a>
+
 ## Adding WebSites
 
-To add web sites and url/routing to the application use the provided PowerShell script
+To add web sites and url/routing to the application gateway use the provided PowerShell script
+
+### Adding a Azure web site
+```powershell
+.\AddSiteRoute.ps1 -resourceGroupName MyResourceGroup -applicationGatewayName MyAppGateway -webSiteName "MyCustomersApi" -paths "/api/customers","/api/customers/*"
+```
+
+This will route &lt;applicationGateway-ip-or-fqdns&gt;/api/customers to "MyCustomersApi.azurewebsites.net/api/customers"
+
+### Adding an external web site
+```powershell
+AddSiteRoute.ps1 -resourceGroupName MyResourceGroup -applicationGatewayName MyAppGateway -externalURI "thecatapi.com" -paths "/api/categories","/api/categories/*"
+```
+
+This will route the &lt;applicationGateway-ip-or-fqdns&gt;/api/categories/list to http://thecatapi.com/api/categories/list
